10ea1f3424448751cbb4976b9c2fa91c0238d7e1
shayan ghazali <shayan97.tti@gmail.com>
2023-05-12 23:59:46 +0430

    particle.js & typed done

--- projects/apps/resume-portfolio/src/app/home/home.component.ts
+++ projects/apps/resume-portfolio/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 
 @Component({
@@ -6,8 +6,25 @@ import { Title } from '@angular/platform-browser';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
   constructor(private title:Title){
     title.setTitle($localize`Home`);
   }
+
+  ngOnInit(): void {
+    const options = {
+      strings: [$localize`Create a well-designed portfolio ^7000`, $localize`Online business support and personalization ^10000`],
+      typeSpeed: 65,
+      loop: true
+    };
+
+    // @ts-ignore
+    particlesJS.load('hero', './assets/js/particles-config.json', null);
+
+    // @ts-ignore
+    new Typed('#hero .message h1', options);
+
+  }
+
 }
